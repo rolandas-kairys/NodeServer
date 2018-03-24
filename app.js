4ca32824eb9558ae86bf7ce87db6f275aa7f45c4
rolandas-kairys <rolandas.kairys@gmail.com>
2018-03-24 08:49:38 +0200

    importing some data to the project

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const routes = require('./routes/index.js');
 // creating an instance of express and storing in variable
 const app = express();
 
+const dataFile = require('./data/data.json');
+
+
 // and the using PUG
 app.set('view engine', 'pug');
 app.set('views', './views');
